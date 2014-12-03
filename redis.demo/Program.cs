381950eeebdcd9d36902daee68e539760047476a
姚耀 <yaoyao12306@163.com>
2014-12-03 23:00:55 +0800

    merge redis.demo

---
+++ redis.demo/Program.cs
@@ -0,0 +1,16 @@
+﻿using StackExchange.Redis;
+
+namespace redis.demo
+{
+    internal class Program
+    {
+        private static void Main(string[] args)
+        {
+            var redis = ConnectionMultiplexer.Connect("localhost");
+            var database = redis.GetDatabase();
+            database.StringSet("test", "test2");
+            var result = database.StringGet("test");
+            redis.Close();
+        }
+    }
+}
