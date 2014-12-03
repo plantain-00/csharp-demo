4e6f0224eed3ff3257edb3367b023868eed6949a
姚耀 <yaoyao12306@163.com>
2014-12-03 22:45:31 +0800

    merge rabbitmq.demo

---
+++ RabbitMQ.Demo/Program.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Text;
+
+using RabbitMQ.Client;
+
+namespace RabbitMQ.Demo
+{
+    internal class Program
+    {
+        private static void Main(string[] args)
+        {
+            ConnectionFactory factory = new ConnectionFactory();
+            IConnection conn = factory.CreateConnection();
+            var channel = conn.CreateModel();
+
+            var exchangeName = "exchangeName";
+            channel.ExchangeDeclare(exchangeName, ExchangeType.Direct);
+            var queueName = "queueName";
+            channel.QueueDeclare(queueName, false, false, false, null);
+            var routingKey = "";
+            channel.QueueBind(queueName, exchangeName, routingKey, null);
+
+            var messageBodyBytes = Encoding.UTF8.GetBytes(Guid.NewGuid().ToString());
+            channel.BasicPublish(exchangeName, routingKey, null, messageBodyBytes);
+
+            bool noAck = false;
+            BasicGetResult result = channel.BasicGet(queueName, noAck);
+            if (result == null)
+            {
+                // No message available at this time.
+            }
+            else
+            {
+                IBasicProperties props = result.BasicProperties;
+                byte[] body = result.Body;
+                var message = Encoding.UTF8.GetString(body);
+                channel.BasicAck(result.DeliveryTag, false);
+            }
+
+            channel.Close(200, "Goodbye");
+            conn.Close();
+        }
+    }
+}
