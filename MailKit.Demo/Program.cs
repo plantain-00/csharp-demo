cd028c6ca1e04b8b8bd774eeed67dbc7b66f5e8f
姚耀 <yaoyao12306@163.com>
2014-12-03 22:40:50 +0800

    merge mailkit.demo

---
+++ MailKit.Demo/Program.cs
@@ -0,0 +1,36 @@
+﻿using MailKit.Net.Smtp;
+
+using MimeKit;
+
+namespace MailKit.Demo
+{
+    internal class Program
+    {
+        private static void Main(string[] args)
+        {
+            var message = new MimeMessage();
+            message.From.Add(new MailboxAddress("mailkit_test", "mailkit_test@163.com"));
+            message.To.Add(new MailboxAddress("yaoyao12306", "yaoyao12306@163.com"));
+            message.Subject = "How you doin'?";
+
+            message.Body = new TextPart("plain")
+                           {
+                               Text = @"Hey Chandler,
+
+I just wanted to let you know that Monica and I were going to go play some paintball, you in?
+
+-- Joey"
+                           };
+
+            using (var client = new SmtpClient())
+            {
+                client.Connect("smtp.163.com");
+                client.AuthenticationMechanisms.Remove("XOAUTH2");
+                client.Authenticate("mailkit_test", "mailkit_password");
+
+                client.Send(message);
+                client.Disconnect(true);
+            }
+        }
+    }
+}
---
+++ MailKit.Template.Demo/Controllers/HomeController.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+using Bootstrap.Pagination;
+
+using MailKit.Net.Smtp;
+
+using MimeKit;
+
+namespace MailKit.Template.Demo.Controllers
+{
+    public class HomeController : Controller
+    {
+        //
+        // GET: /Home/
+
+        public ActionResult Index()
+        {
+            ViewData["name"] = "Monica";
+
+            var message = new MimeMessage();
+            message.From.Add(new MailboxAddress("mailkit_test", "mailkit_test@163.com"));
+            message.To.Add(new MailboxAddress("yaoyao12306", "yaoyao12306@163.com"));
+            message.Subject = this.PartialViewToString("NotifySubject");
+            message.Body = new TextPart("plain")
+            {
+                Text = this.PartialViewToString("NotifyBody")
+            };
+
+            using (var client = new SmtpClient())
+            {
+                client.Connect("smtp.163.com");
+                client.AuthenticationMechanisms.Remove("XOAUTH2");
+                client.Authenticate("mailkit_test", "mailkit_password");
+
+                client.Send(message);
+                client.Disconnect(true);
+            }
+            return View();
+        }
+
+    }
+}
