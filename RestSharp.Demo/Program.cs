e67fa1bcd2e7ac7c75ade1d9d61b8ae7994f417d
姚耀 <yaoyao12306@163.com>
2014-12-03 22:38:57 +0800

    merge restsharp.demo

---
+++ RestSharp.Demo/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+namespace RestSharp.Demo
+{
+    internal class Program
+    {
+        private static void Main(string[] args)
+        {
+            var client = new RestClient("https://api.github.com/gists");
+            var request = new RestRequest();
+            var response = client.Execute(request);
+            var content = response.Content;
+            Console.WriteLine(content);
+            Console.Read();
+        }
+    }
+}
