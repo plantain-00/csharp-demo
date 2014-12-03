2485a9ee28ea24d8aeb9a939e9a1363b97cef903
姚耀 <yaoyao12306@163.com>
2014-12-03 22:30:49 +0800

    merge octokit.demo

---
+++ octokit.demo/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Threading.Tasks;
+
+using Octokit;
+
+namespace octokit.demo
+{
+    internal class Program
+    {
+        private static void Main(string[] args)
+        {
+            var github = new GitHubClient(new ProductHeaderValue("MyAmazingApp"));
+            var task = github.User.Get("half-ogre");
+            var user = task.Result;
+            Console.WriteLine(user.Followers + " folks love the half ogre!");
+            Console.Read();
+        }
+    }
+}
