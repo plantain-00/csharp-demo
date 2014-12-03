3973f203ceae5d5bfe9000f7ce762bff291f1ad5
姚耀 <yaoyao12306@163.com>
2014-12-03 22:43:41 +0800

    merge lightgit2sharp.demo

---
+++ Libgit2Sharp.Demo/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+using LibGit2Sharp;
+
+namespace Libgit2Sharp.Demo
+{
+    internal class Program
+    {
+        private static void Main(string[] args)
+        {
+            var result = Repository.Clone("https://github.com/plantain-00/resume", @"C:\Users\York Yao\Documents\test");
+            Console.Read();
+        }
+    }
+}
