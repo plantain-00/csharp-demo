8a19aca19928224bf5d6d1e924d593c213453c16
姚耀 <yaoyao12306@163.com>
2014-12-03 22:59:24 +0800

    merge jsonp.demo

---
+++ jsonp.provider/Controllers/HomeController.cs
@@ -0,0 +1,20 @@
+﻿using System.Web.Mvc;
+
+namespace jsonp.provider.Controllers
+{
+    public class HomeController : Controller
+    {
+        public ActionResult Index()
+        {
+            return new JsonpResult
+                   {
+                       Data = new
+                              {
+                                  Name = "Irving",
+                                  Age = 23
+                              },
+                       JsonRequestBehavior = JsonRequestBehavior.AllowGet
+                   };
+        }
+    }
+}
---
+++ jsonp.provider/JsonpResult.cs
@@ -0,0 +1,27 @@
+﻿using System.Web.Mvc;
+
+namespace jsonp.provider
+{
+    public class JsonpResult : JsonResult
+    {
+        public JsonpResult()
+        {
+            JsonRequestBehavior = JsonRequestBehavior.AllowGet;
+        }
+
+        public string Callback { get; set; }
+
+        public override void ExecuteResult(ControllerContext context)
+        {
+            var httpContext = context.HttpContext;
+            var callback = Callback;
+            if (string.IsNullOrWhiteSpace(callback))
+            {
+                callback = httpContext.Request["callback"];
+            }
+            httpContext.Response.Write(callback + "(");
+            base.ExecuteResult(context);
+            httpContext.Response.Write(");");
+        }
+    }
+}
