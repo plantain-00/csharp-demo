49f3d1f9b1f045fe4f639f2fdf798d5bd4968922
姚耀 <yaoyao12306@163.com>
2014-12-03 22:33:27 +0800

    merge sharpziplib.demo

---
+++ SharpZipLib.Demo/Program.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.IO;
+
+using ICSharpCode.SharpZipLib.Zip;
+
+namespace SharpZipLib.Demo
+{
+    internal class Program
+    {
+        private static void Main(string[] args)
+        {
+            var filenames = Directory.GetFiles("../../");
+            using (var s = new ZipOutputStream(File.Create("result.zip")))
+            {
+                s.SetLevel(9);
+                var buffer = new byte[4096];
+                foreach (var file in filenames)
+                {
+                    var entry = new ZipEntry(Path.GetFileName(file))
+                                {
+                                    DateTime = DateTime.Now
+                                };
+                    s.PutNextEntry(entry);
+                    using (var fs = File.OpenRead(file))
+                    {
+                        int sourceBytes;
+                        do
+                        {
+                            sourceBytes = fs.Read(buffer, 0, buffer.Length);
+                            s.Write(buffer, 0, sourceBytes);
+                        }
+                        while (sourceBytes > 0);
+                    }
+                }
+            }
+        }
+    }
+}
