d9879c114fdcf71328a3c571452db40fc3547492
姚耀 <yaoyao12306@163.com>
2014-12-04 08:20:17 +0800

    merge cppsharp.demo

---
+++ CppSharp.Demo/Program.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using CppSharp.AST;
+using CppSharp.Generators;
+
+namespace CppSharp.Demo
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            ConsoleDriver.Run(new SampleLibrary());
+            Console.Read();
+        }
+    }
+
+    internal class SampleLibrary : ILibrary
+    {
+        public void Preprocess(Driver driver, ASTContext ctx)
+        {
+        }
+
+        public void Postprocess(Driver driver, ASTContext ctx)
+        {
+        }
+
+        public void Setup(Driver driver)
+        {
+            var options = driver.Options;
+            options.GeneratorKind = GeneratorKind.CSharp;
+            options.LibraryName = "Sample";
+            options.Headers.Add("C:\\Users\\York Yao\\Documents\\Sample.h");
+            //options.Libraries.Add("Sample.lib");
+        }
+
+        public void SetupPasses(Driver driver)
+        {
+        }
+    }
+}
