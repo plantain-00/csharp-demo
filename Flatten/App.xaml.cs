43ca0f08f81c7b8f1ef96f7553989abad5075f27
姚耀 <yaoyao12306@163.com>
2014-12-04 08:03:15 +0800

    merge flatten

---
+++ Flatten/App.xaml.cs
@@ -0,0 +1,25 @@
+﻿using System.Windows;
+
+namespace Flatten
+{
+    /// <summary>
+    /// App.xaml 的交互逻辑
+    /// </summary>
+    public partial class App
+    {
+        protected override void OnStartup(StartupEventArgs e)
+        {
+            SingleInstanceAppliction.SetGuid("{313C052C-6AB5-45F4-A689-1F1D51D4B96E}");
+            if (SingleInstanceAppliction.IsFirst())
+            {
+                base.OnStartup(e);
+                SingleInstanceAppliction.ReleaseMutex();
+            }
+            else
+            {
+                SingleInstanceAppliction.PostMessage();
+                Shutdown();
+            }
+        }
+    }
+}
---
+++ Flatten/MainWindow.xaml.cs
@@ -0,0 +1,78 @@
+﻿using System;
+using System.IO;
+using System.Windows;
+using System.Windows.Interop;
+
+using Microsoft.VisualBasic.FileIO;
+
+namespace Flatten
+{
+    /// <summary>
+    ///     MainWindow.xaml 的交互逻辑
+    /// </summary>
+    public partial class MainWindow
+    {
+        protected override void OnSourceInitialized(EventArgs e)
+        {
+            base.OnSourceInitialized(e);
+            var source = PresentationSource.FromVisual(this) as HwndSource;
+            source.AddHook(WndProc);
+        }
+        private IntPtr WndProc(IntPtr hwnd, int msg, IntPtr wParam, IntPtr lParam, ref bool handled)
+        {
+            if (msg == SingleInstanceAppliction.WM_SHOWME)
+            {
+                if (WindowState == WindowState.Minimized)
+                {
+                    WindowState = WindowState.Normal;
+                }
+                var top = Topmost;
+                Topmost = true;
+                Topmost = top;
+            }
+            return IntPtr.Zero;
+        }
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+        private void TextBox_OnPreviewDrop(object sender, DragEventArgs e)
+        {
+            var directories = (string[]) e.Data.GetData(DataFormats.FileDrop);
+            var destination = Path.GetDirectoryName(directories[0]);
+            foreach (var directory in directories)
+            {
+                Move(directory, destination);
+                textBox.Text += directory + "\n";
+            }
+        }
+        private void TextBox_OnPreviewDragOver(object sender, DragEventArgs e)
+        {
+            e.Effects = DragDropEffects.Copy;
+            e.Handled = true;
+        }
+        private static void Move(string path, string destination)
+        {
+            if (Directory.Exists(path))
+            {
+                var files = Directory.GetFiles(path);
+                foreach (var file in files)
+                {
+                    var name = Path.GetFileName(file);
+                    var destinationPath = Path.Combine(destination, name);
+                    if (File.Exists(destinationPath))
+                    {
+                        destinationPath = Path.Combine(destination, Guid.NewGuid() + name);
+                    }
+                    File.Move(file, destinationPath);
+                }
+                var directories = Directory.GetDirectories(path);
+                foreach (var directory in directories)
+                {
+                    Move(directory, destination);
+                }
+                FileSystem.DeleteDirectory(path, UIOption.OnlyErrorDialogs, RecycleOption.SendToRecycleBin);
+            }
+        }
+    }
+}
