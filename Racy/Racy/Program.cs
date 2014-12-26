e55d2ae9010002ed823e28a63d87a02061ac528d
york yao <york.yao@dragonsoftbravo.com>
2014-12-26 15:23:53 +0800

    add Racy state machine

---
+++ Racy/Racy/Program.cs
@@ -0,0 +1,50 @@
+﻿using System;
+
+namespace Racy
+{
+    public enum State
+    {
+        OffHook,
+        Ringing,
+        Connected,
+        OnHold
+    }
+
+    public enum Trigger
+    {
+        HungUp,
+        CallConnected,
+        LeftMessage,
+        PlacedOnHold,
+        CallDialled
+    }
+
+    internal class Program
+    {
+        private static void Main(string[] args)
+        {
+            var phoneCall = new StateMachine<State, Trigger>(State.OffHook);
+
+            phoneCall[State.OffHook].CanBeFired(Trigger.CallDialled).To(State.Ringing);
+
+            phoneCall[State.Ringing].CanBeFired(Trigger.HungUp).To(State.OffHook);
+            phoneCall[State.Ringing].CanBeFired(Trigger.CallConnected).To(State.Connected);
+
+            phoneCall[State.Connected].OnEntry += (s1, t, s2) => Console.WriteLine("entry {0} from {1} by {2}", s2, s1, t);
+            phoneCall[State.Connected].OnExit += (s1, t, s2) => Console.WriteLine("exit {0} to {1} by {2}", s1, s2, t);
+            phoneCall[State.Connected].CanBeFired(Trigger.LeftMessage).To(State.OffHook);
+            phoneCall[State.Connected].CanBeFired(Trigger.HungUp).To(State.OffHook);
+            phoneCall[State.Connected].CanBeFired(Trigger.PlacedOnHold).To(State.OnHold);
+
+            phoneCall.Fire(Trigger.CallDialled);
+            Console.WriteLine(phoneCall.State);
+
+            phoneCall.Fire(Trigger.CallConnected);
+            Console.WriteLine(phoneCall.State);
+
+            phoneCall.Fire(Trigger.LeftMessage);
+            Console.WriteLine(phoneCall.State);
+            Console.Read();
+        }
+    }
+}
---
+++ Racy/Racy/StateMachine.cs
@@ -0,0 +1,119 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace Racy
+{
+    public class StateMachine<TState, TTrigger> where TState : struct where TTrigger : struct
+    {
+        private readonly List<StateConfiguration<TState, TTrigger>> _configurations;
+
+        public StateMachine(TState state)
+        {
+            State = state;
+            _configurations = new List<StateConfiguration<TState, TTrigger>>();
+        }
+
+        public StateConfiguration<TState, TTrigger> this[TState state]
+        {
+            get
+            {
+                var config = _configurations.FirstOrDefault(c => Equals(c.State, state));
+                if (config == null)
+                {
+                    config = new StateConfiguration<TState, TTrigger>(state);
+                    _configurations.Add(config);
+                }
+                return config;
+            }
+        }
+
+        public TState State { get; private set; }
+
+        public bool CanFire(TTrigger trigger)
+        {
+            var firer = GetFirer(trigger);
+            return firer != null;
+        }
+
+        private TriggerFirer<TState, TTrigger> GetFirer(TTrigger trigger)
+        {
+            var config = _configurations.FirstOrDefault(c => c.State.Equals(State));
+            if (config == null)
+            {
+                return null;
+            }
+            var firer = config.TriggerFirers.FirstOrDefault(t => t.Trigger.Equals(trigger) && t.State != null);
+            return firer;
+        }
+
+        public void Fire(TTrigger trigger)
+        {
+            var firer = GetFirer(trigger);
+            if (firer == null
+                || firer.State == null)
+            {
+                throw new Exception("cannot fire the trigger at current state.");
+            }
+
+            var config = _configurations.FirstOrDefault(c => c.State.Equals(State));
+            if (config != null
+                && config.OnExit != null)
+            {
+                config.OnExit(State, trigger, firer.State.Value);
+            }
+
+            var lastState = State;
+            State = firer.State.Value;
+
+            config = _configurations.FirstOrDefault(c => c.State.Equals(State));
+            if (config != null
+                && config.OnExit != null)
+            {
+                config.OnEntry(lastState, trigger, State);
+            }
+        }
+    }
+
+    public class StateConfiguration<TState, TTrigger> where TState : struct where TTrigger : struct
+    {
+        internal readonly TState State;
+        internal readonly List<TriggerFirer<TState, TTrigger>> TriggerFirers;
+
+        public Action<TState, TTrigger, TState> OnEntry;
+        public Action<TState, TTrigger, TState> OnExit;
+
+        internal StateConfiguration(TState state)
+        {
+            State = state;
+            TriggerFirers = new List<TriggerFirer<TState, TTrigger>>();
+        }
+
+        public TriggerFirer<TState, TTrigger> CanBeFired(TTrigger trigger)
+        {
+            var triggerFirer = TriggerFirers.FirstOrDefault(t => t.Trigger.Equals(trigger));
+            if (triggerFirer == null)
+            {
+                triggerFirer = new TriggerFirer<TState, TTrigger>(trigger);
+                TriggerFirers.Add(triggerFirer);
+            }
+            return triggerFirer;
+        }
+    }
+
+    public class TriggerFirer<TState, TTrigger> where TState : struct where TTrigger : struct
+    {
+        internal readonly TTrigger Trigger;
+        internal TState? State;
+
+        internal TriggerFirer(TTrigger trigger)
+        {
+            Trigger = trigger;
+        }
+
+        public void To(TState state)
+        {
+            State = state;
+        }
+    }
+}
