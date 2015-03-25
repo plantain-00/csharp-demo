e1ae6fd9458995c1820fa6ed358dd000d761e204
yaoyao <yaoyao12306@163.com>
2015-03-25 18:18:52 +0800

    add single-linked list reverse

---
+++ SingleLinkedListReverse/SingleLinkedListReverse/Program.cs
@@ -0,0 +1,47 @@
+﻿namespace SingleLinkedListReverse
+{
+    internal class Program
+    {
+        private static void Main(string[] args)
+        {
+            var node = new Node<int>
+                       {
+                           Value = 0,
+                           NextNode = new Node<int>
+                                      {
+                                          Value = 1,
+                                          NextNode = new Node<int>
+                                                     {
+                                                         Value = 2,
+                                                         NextNode = new Node<int>
+                                                                    {
+                                                                        Value = 3
+                                                                    }
+                                                     }
+                                      }
+                       };
+            var newNode = node.Reverse();
+        }
+    }
+
+    public class Node<T>
+    {
+        public T Value { get; set; }
+        public Node<T> NextNode { get; set; }
+
+        public Node<T> Reverse()
+        {
+            var result = this;
+            var current = this;
+            while (NextNode != null)
+            {
+                result = NextNode;
+                NextNode = result.NextNode;
+                result.NextNode = current;
+                current = result;
+            }
+
+            return result;
+        }
+    }
+}
