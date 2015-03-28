06311f6f17535975e61a3328c89ded431c677fdf
yaoyao <yaoyao12306@163.com>
2015-03-28 10:31:47 +0800

    add argo 191

---
+++ Playthings/LeetCode.Solutions/LeetCode.Solutions.Argorithms/_191_NumberOf1Bits.cs
@@ -0,0 +1,16 @@
+﻿namespace LeetCode.Solutions.Argorithms
+{
+    public class _191_NumberOf1Bits
+    {
+        public int HammingWeight(uint n)
+        {
+            var result = 0;
+            while (n != 0)
+            {
+                n = n & (n - 1);
+                result++;
+            }
+            return result;
+        }
+    }
+}
---
+++ Playthings/LeetCode.Solutions/_191_NumberOf1Bits/Class1.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _191_NumberOf1Bits
+{
+    public class Class1
+    {
+    }
+}
