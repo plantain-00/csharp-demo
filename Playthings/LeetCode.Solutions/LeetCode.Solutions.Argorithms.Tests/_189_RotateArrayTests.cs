c2fadb79492ff58f03172708afa97d14e4055959
yaoyao <yaoyao12306@163.com>
2015-03-28 15:42:24 +0800

    add 189#

---
+++ Playthings/LeetCode.Solutions/LeetCode.Solutions.Argorithms.Tests/_189_RotateArrayTests.cs
@@ -0,0 +1,87 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace LeetCode.Solutions.Argorithms.Tests
+{
+    [TestClass]
+    public class _189_RotateArrayTests
+    {
+        [TestMethod]
+        public void RotateTest()
+        {
+            var array = new[]
+                        {
+                            1,
+                            2,
+                            3,
+                            4,
+                            5,
+                            6,
+                            7
+                        };
+            new _189_RotateArray().Rotate(array, 3);
+            Assert.IsTrue(array[0] == 5);
+            Assert.IsTrue(array[1] == 6);
+            Assert.IsTrue(array[2] == 7);
+
+            Assert.IsTrue(array[3] == 1);
+            Assert.IsTrue(array[4] == 2);
+            Assert.IsTrue(array[5] == 3);
+            Assert.IsTrue(array[6] == 4);
+
+            array = new[]
+                    {
+                        1,
+                        2,
+                        3,
+                        4,
+                        5,
+                        6,
+                        7,
+                        8
+                    };
+            new _189_RotateArray().Rotate(array, 2);
+            Assert.IsTrue(array[0] == 7);
+            Assert.IsTrue(array[1] == 8);
+
+            Assert.IsTrue(array[2] == 1);
+            Assert.IsTrue(array[3] == 2);
+            Assert.IsTrue(array[4] == 3);
+            Assert.IsTrue(array[5] == 4);
+            Assert.IsTrue(array[6] == 5);
+            Assert.IsTrue(array[7] == 6);
+
+            array = new[]
+                    {
+                        1
+                    };
+            new _189_RotateArray().Rotate(array, 0);
+            Assert.IsTrue(array[0] == 1);
+
+            array = new[]
+                    {
+                        1,
+                        2
+                    };
+            new _189_RotateArray().Rotate(array, 3);
+            Assert.IsTrue(array[0] == 2);
+            Assert.IsTrue(array[1] == 1);
+
+            array = new[]
+                    {
+                        1,
+                        2,
+                        3,
+                        4,
+                        5,
+                        6
+                    };
+            new _189_RotateArray().Rotate(array, 4);
+            Assert.IsTrue(array[0] == 3);
+            Assert.IsTrue(array[1] == 4);
+            Assert.IsTrue(array[2] == 5);
+            Assert.IsTrue(array[3] == 6);
+            Assert.IsTrue(array[4] == 1);
+            Assert.IsTrue(array[5] == 2);
+        }
+    }
+}
---
+++ Playthings/LeetCode.Solutions/LeetCode.Solutions.Argorithms/_189_RotateArray.cs
@@ -0,0 +1,75 @@
+﻿namespace LeetCode.Solutions.Argorithms
+{
+    public class _189_RotateArray
+    {
+        public void Rotate(int[] nums, int k)
+        {
+            if (k >= nums.Length)
+            {
+                k %= nums.Length;
+            }
+            if (k == 0)
+            {
+                return;
+            }
+            var common = GetGreatestCommonDivisor(nums.Length, k);
+
+            var loop = nums.Length / common;
+            for (var i = 0; i < common; i++)
+            {
+                var tmp = nums[i];
+                var index = i;
+                for (var j = 0; j < loop; j++)
+                {
+                    if (j == loop - 1)
+                    {
+                        nums[index] = tmp;
+                    }
+                    else
+                    {
+                        var newIndex = GetLastIndex(index, nums.Length, k);
+                        nums[index] = nums[newIndex];
+                        index = newIndex;
+                    }
+                }
+            }
+        }
+
+        private static int GetLastIndex(int index, int n, int k)
+        {
+            if (index < k)
+            {
+                return index + n - k;
+            }
+            return index - k;
+        }
+
+        private static int GetGreatestCommonDivisor(int a, int b)
+        {
+            int larger;
+            int smaller;
+            if (a == b)
+            {
+                return a;
+            }
+            if (a > b)
+            {
+                larger = a;
+                smaller = b;
+            }
+            else
+            {
+                larger = b;
+                smaller = a;
+            }
+            var divider = b;
+            while (divider != 0)
+            {
+                divider = larger % smaller;
+                larger = smaller;
+                smaller = divider;
+            }
+            return larger;
+        }
+    }
+}
