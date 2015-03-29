e804a1a0f82c8b8857891d09901b9630dcdc2454
yaoyao <yaoyao12306@163.com>
2015-03-29 15:04:12 +0800

    add 174#

---
+++ Playthings/LeetCode.Solutions/LeetCode.Solutions.Argorithms.Tests/_174_DungeonGameTests.cs
@@ -0,0 +1,32 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace LeetCode.Solutions.Argorithms.Tests
+{
+    [TestClass]
+    public class _174_DungeonGameTests
+    {
+        [TestMethod]
+        public void CalculateMinimumHPTest()
+        {
+            var array = new[,]
+                        {
+                            {
+                                -2,
+                                -3,
+                                3
+                            },
+                            {
+                                -5,
+                                -10,
+                                1
+                            },
+                            {
+                                10,
+                                30,
+                                -5
+                            }
+                        };
+            Assert.IsTrue(new _174_DungeonGame().CalculateMinimumHP(array) == 7);
+        }
+    }
+}
