bb3240bf990d735a4a595a7e24c85e6c0ed1bc7e
yaoyao <yaoyao12306@163.com>
2015-03-29 09:51:55 +0800

    add 187#

---
+++ Playthings/LeetCode.Solutions/LeetCode.Solutions.Argorithms.Tests/_187_RepeatedDNASequencesTests.cs
@@ -0,0 +1,23 @@
+﻿using System.Linq;
+
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace LeetCode.Solutions.Argorithms.Tests
+{
+    [TestClass]
+    public class _187_RepeatedDNASequencesTests
+    {
+        [TestMethod]
+        public void FindRepeatedDnaSequencesTest()
+        {
+            var result = new _187_RepeatedDNASequences().FindRepeatedDnaSequences("AAAAACCCCCAAAAACCCCCCAAAAAGGGTTT").ToArray();
+            Assert.IsTrue(result.Length == 2);
+            Assert.IsTrue(result[0] == "AAAAACCCCC");
+            Assert.IsTrue(result[1] == "CCCCCAAAAA");
+
+            result = new _187_RepeatedDNASequences().FindRepeatedDnaSequences("AAAAAAAAAAA").ToArray();
+            Assert.IsTrue(result.Length == 1);
+            Assert.IsTrue(result[0] == "AAAAAAAAAA");
+        }
+    }
+}
---
+++ Playthings/LeetCode.Solutions/LeetCode.Solutions.Argorithms/_187_RepeatedDNASequences.cs
@@ -0,0 +1,61 @@
+﻿using System.Collections.Generic;
+
+namespace LeetCode.Solutions.Argorithms
+{
+    public class _187_RepeatedDNASequences
+    {
+        public IList<string> FindRepeatedDnaSequences(string s)
+        {
+            if (s.Length <= 10)
+            {
+                return new string[0];
+            }
+            var result = new List<string>();
+            var dictionary = new Dictionary<int, bool>();
+            for (var i = 0; i < s.Length - 9; i++)
+            {
+                var tmp = ToInt(s, i);
+                if (dictionary.ContainsKey(tmp))
+                {
+                    if (dictionary[tmp] == false)
+                    {
+                        dictionary[tmp] = true;
+                        result.Add(s.Substring(i, 10));
+                    }
+                }
+                else
+                {
+                    dictionary.Add(tmp, false);
+                }
+            }
+            return result;
+        }
+
+        private static int ToInt(string s, int index)
+        {
+            var result = 0;
+            for (var i = index; i < index + 10; i++)
+            {
+                int bit;
+                switch (s[i])
+                {
+                    case 'A':
+                        bit = 0;
+                        break;
+                    case 'C':
+                        bit = 1;
+                        break;
+                    case 'G':
+                        bit = 2;
+                        break;
+                    default:
+                        bit = 3;
+                        break;
+                }
+                result <<= 2;
+                result |= bit;
+            }
+            return result;
+        }
+    }
+}
