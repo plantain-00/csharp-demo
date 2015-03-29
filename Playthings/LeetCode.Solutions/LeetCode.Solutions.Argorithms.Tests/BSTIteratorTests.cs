fbc69dec4dce5e91ffa53adf9c0a0fe564f874a3
yaoyao <yaoyao12306@163.com>
2015-03-30 07:59:03 +0800

    add test

---
+++ Playthings/LeetCode.Solutions/LeetCode.Solutions.Argorithms.Tests/BSTIteratorTests.cs
@@ -0,0 +1,36 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace LeetCode.Solutions.Argorithms.Tests
+{
+    [TestClass]
+    public class BSTIteratorTests
+    {
+        [TestMethod]
+        public void NextTest()
+        {
+            var tree = new TreeNode(4)
+                       {
+                           left = new TreeNode(2)
+                                  {
+                                      left = new TreeNode(1),
+                                      right = new TreeNode(3)
+                                  },
+                           right = new TreeNode(6)
+                                   {
+                                       left = new TreeNode(5),
+                                       right = new TreeNode(7)
+                                   }
+                       };
+
+            var iterator = new BSTIterator(tree);
+
+            var i = 0;
+
+            while (iterator.HasNext())
+            {
+                i++;
+                Assert.IsTrue(iterator.Next() == i);
+            }
+        }
+    }
+}
