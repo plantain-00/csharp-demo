c9206ee5919cf839df34a94919b8e203dcf13ca3
yaoyao <yaoyao12306@163.com>
2015-03-30 09:09:15 +0800

    save

---
+++ Playthings/LeetCode.Solutions/LeetCode.Solutions.Argorithms.Tests/_162_FindPeakElementTests.cs
@@ -0,0 +1,21 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace LeetCode.Solutions.Argorithms.Tests
+{
+    [TestClass]
+    public class _162_FindPeakElementTests
+    {
+        [TestMethod]
+        public void FindPeakElementTest()
+        {
+            var array = new[]
+                        {
+                            1,
+                            2,
+                            3,
+                            1
+                        };
+            Assert.IsTrue(new _162_FindPeakElement().FindPeakElement(array) == 2);
+        }
+    }
+}
