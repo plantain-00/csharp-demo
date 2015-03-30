56e21f09d1c2e927efd8a13fab6d227dff5c3f19
yaoyao <yaoyao12306@163.com>
2015-03-30 08:49:45 +0800

    save

---
+++ Playthings/LeetCode.Solutions/LeetCode.Solutions.Argorithms.Tests/_168_ExcelSheetColumnTitleTests.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace LeetCode.Solutions.Argorithms.Tests
+{
+    [TestClass]
+    public class _168_ExcelSheetColumnTitleTests
+    {
+        [TestMethod]
+        public void ConvertToTitleTest()
+        {
+            Assert.IsTrue(new _168_ExcelSheetColumnTitle().ConvertToTitle(1) == "A");
+        }
+    }
+}
---
+++ Playthings/LeetCode.Solutions/LeetCode.Solutions.Argorithms/_168_ExcelSheetColumnTitle.cs
@@ -0,0 +1,17 @@
+﻿namespace LeetCode.Solutions.Argorithms
+{
+    public class _168_ExcelSheetColumnTitle
+    {
+        public string ConvertToTitle(int n)
+        {
+            var result = "";
+            while (n != 0)
+            {
+                n--;
+                result = (char)('A' + n % 26) + result;
+                n /= 26;
+            }
+            return result;
+        }
+    }
+}
