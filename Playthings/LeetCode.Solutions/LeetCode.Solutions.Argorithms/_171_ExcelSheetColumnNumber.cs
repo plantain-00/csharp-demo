235ddc9c9084953f2b66815db364d81d07943493
yaoyao <yaoyao12306@163.com>
2015-03-30 08:08:49 +0800

    save

---
+++ Playthings/LeetCode.Solutions/LeetCode.Solutions.Argorithms/_171_ExcelSheetColumnNumber.cs
@@ -0,0 +1,17 @@
+﻿namespace LeetCode.Solutions.Argorithms
+{
+    public class _171_ExcelSheetColumnNumber
+    {
+        public int TitleToNumber(string s)
+        {
+            var colNumber = 0;
+
+            foreach (var c in s)
+            {
+                colNumber = colNumber * 26 + (c - 64);
+            }
+
+            return colNumber;
+        }
+    }
+}
---
+++ Playthings/LeetCode.Solutions/LeetCode.Solutions.Argorithms/_172_TrailingZeroes.cs
@@ -0,0 +1,14 @@
+﻿namespace LeetCode.Solutions.Argorithms
+{
+    public class _172_TrailingZeroes
+    {
+        public int TrailingZeroes(int n)
+        {
+            if (n < 5)
+            {
+                return 0;
+            }
+            return n / 5 + TrailingZeroes(n / 5);
+        }
+    }
+}
