2e39f5703f66b2016ef0257284e41edd8ab2b514
yaoyao <yaoyao12306@163.com>
2015-04-09 08:46:24 +0800

    save

--- Playthings/LeetCode.Solutions/LeetCode.Solutions.Argorithms/_199_BinaryTreeRightSideView.cs
+++ Playthings/LeetCode.Solutions/LeetCode.Solutions.Argorithms/_199_BinaryTreeRightSideView.cs
@@ -1,35 +1,29 @@
-﻿using System.Collections.Generic;
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
 
 namespace LeetCode.Solutions.Argorithms
 {
     public class _199_BinaryTreeRightSideView
     {
-        private int _current;
-
         public IList<int> RightSideView(TreeNode root)
         {
-            var result = new List<int>();
-            Gen(root, 1, result);
-            return result;
-        }
-
-        private void Gen(TreeNode root, int level, ICollection<int> result)
-        {
-            while (true)
+            if (root == null)
             {
-                if (root == null)
-                {
-                    return;
-                }
-                if (level > _current)
-                {
-                    result.Add(root.val);
-                    _current = level;
-                }
-                Gen(root.right, level + 1, result);
-                root = root.left;
-                level = level + 1;
+                return new List<int>();
             }
+            var left = RightSideView(root.left);
+            var right = RightSideView(root.right);
+            var result = new List<int>
+                         {
+                             root.val
+                         };
+            result.AddRange(right);
+            if (left.Count > right.Count)
+            {
+                result.AddRange(left.Skip(right.Count));
+            }
+            return result;
         }
     }
 }
