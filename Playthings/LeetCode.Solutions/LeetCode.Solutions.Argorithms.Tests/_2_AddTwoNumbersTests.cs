88c362fb125c0afbc9a363d10ea783dc8add13cd
yaoyao <yaoyao12306@163.com>
2015-04-19 16:52:11 +0800

    save

---
+++ Playthings/LeetCode.Solutions/LeetCode.Solutions.Argorithms.Tests/_2_AddTwoNumbersTests.cs
@@ -0,0 +1,32 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace LeetCode.Solutions.Argorithms.Tests
+{
+    [TestClass]
+    public class _2_AddTwoNumbersTests
+    {
+        [TestMethod]
+        public void AddTwoNumbersTest()
+        {
+            var l1 = new ListNode(2)
+                     {
+                         next = new ListNode(4)
+                                {
+                                    next = new ListNode(3)
+                                }
+                     };
+            var l2 = new ListNode(5)
+                     {
+                         next = new ListNode(6)
+                                {
+                                    next = new ListNode(4)
+                                }
+                     };
+            var result = new _2_AddTwoNumbers().AddTwoNumbers(l1, l2);
+            Assert.IsTrue(result.val == 7);
+            Assert.IsTrue(result.next.val == 0);
+            Assert.IsTrue(result.next.next.val == 8);
+            Assert.IsTrue(result.next.next.next == null);
+        }
+    }
+}
