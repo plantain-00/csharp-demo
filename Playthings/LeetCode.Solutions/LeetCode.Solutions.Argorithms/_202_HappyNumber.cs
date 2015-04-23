240bbf743607ad176a723af0510b87d6b93742de
yaoyao <yaoyao12306@163.com>
2015-04-23 08:53:12 +0800

    save

---
+++ Playthings/LeetCode.Solutions/LeetCode.Solutions.Argorithms/_202_HappyNumber.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections.Generic;
+
+namespace LeetCode.Solutions.Argorithms
+{
+    public class _202_HappyNumber
+    {
+        public bool IsHappy(int n)
+        {
+            var set = new List<int>
+                      {
+                          n
+                      };
+            while (n != 1)
+            {
+                var result = 0;
+                while (n != 0)
+                {
+                    var tmp = n % 10;
+                    result += tmp * tmp;
+                    n /= 10;
+                }
+                if (set.Contains(result))
+                {
+                    return false;
+                }
+                set.Add(result);
+                n = result;
+            }
+            return true;
+        }
+    }
+}
