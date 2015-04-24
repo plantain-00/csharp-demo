2300b0560a98f15b1bd8a3cf180a93c06d3c919f
yaoyao <yaoyao12306@163.com>
2015-04-24 08:58:20 +0800

    save

---
+++ Playthings/LeetCode.Solutions/LeetCode.Solutions.Argorithms.Tests/_203_RemoveLinkedListElementsTests.cs
@@ -0,0 +1,39 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace LeetCode.Solutions.Argorithms.Tests
+{
+    [TestClass]
+    public class _203_RemoveLinkedListElementsTests
+    {
+        [TestMethod]
+        public void RemoveElementsTest()
+        {
+            var list = new ListNode(1)
+                       {
+                           next = new ListNode(2)
+                                  {
+                                      next = new ListNode(6)
+                                             {
+                                                 next = new ListNode(3)
+                                                        {
+                                                            next = new ListNode(4)
+                                                                   {
+                                                                       next = new ListNode(5)
+                                                                              {
+                                                                                  next = new ListNode(6)
+                                                                              }
+                                                                   }
+                                                        }
+                                             }
+                                  }
+                       };
+            var result = new _203_RemoveLinkedListElements().RemoveElements(list, 6);
+            Assert.IsTrue(result.val == 1);
+            Assert.IsTrue(result.next.val == 2);
+            Assert.IsTrue(result.next.next.val == 3);
+            Assert.IsTrue(result.next.next.next.val == 4);
+            Assert.IsTrue(result.next.next.next.next.val == 5);
+            Assert.IsTrue(result.next.next.next.next.next == null);
+        }
+    }
+}
