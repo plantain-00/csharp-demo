50016a19475224cc437ad636fc98a4ca1d2a39c4
yaoyao <yaoyao12306@163.com>
2015-04-26 10:58:33 +0800

    save

---
+++ Playthings/LeetCode.Solutions/LeetCode.Solutions.Argorithms.Tests/_3_LongestSubstringWithoutRepeatingCharactersTests.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace LeetCode.Solutions.Argorithms.Tests
+{
+    [TestClass]
+    public class _3_LongestSubstringWithoutRepeatingCharactersTests
+    {
+        [TestMethod]
+        public void LengthOfLongestSubstringTest()
+        {
+            var a = new _3_LongestSubstringWithoutRepeatingCharacters();
+            Assert.IsTrue(a.LengthOfLongestSubstring("abcabcbb") == 3);
+            Assert.IsTrue(a.LengthOfLongestSubstring("bbbbb") == 1);
+        }
+    }
+}
