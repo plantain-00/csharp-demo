9f67aa0e5dad4b9d79948e1878955ad138d7d055
yaoyao <yaoyao12306@163.com>
2015-05-11 09:06:23 +0800

    save

---
+++ Playthings/LeetCode.Solutions/LeetCode.Solutions.Argorithms.Tests/_204_CountPrimesTests.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace LeetCode.Solutions.Argorithms.Tests
+{
+    [TestClass]
+    public class _204_CountPrimesTests
+    {
+        [TestMethod]
+        public void CountPrimesTest()
+        {
+            var x = new _204_CountPrimes();
+            Assert.IsTrue(x.CountPrimes(2) == 0);
+            Assert.IsTrue(x.CountPrimes(3) == 1);
+            Assert.IsTrue(x.CountPrimes(4) == 2);
+            Assert.IsTrue(x.CountPrimes(5) == 2);
+            Assert.IsTrue(x.CountPrimes(6) == 3);
+        }
+    }
+}
