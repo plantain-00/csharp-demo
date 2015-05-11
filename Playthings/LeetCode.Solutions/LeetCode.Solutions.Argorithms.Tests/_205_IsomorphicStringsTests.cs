efd4acc0dce321720dfa4bb9cfcf8d8f6118fb31
yaoyao <yaoyao12306@163.com>
2015-05-11 21:40:02 +0800

    save

---
+++ Playthings/LeetCode.Solutions/LeetCode.Solutions.Argorithms.Tests/_205_IsomorphicStringsTests.cs
@@ -0,0 +1,18 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace LeetCode.Solutions.Argorithms.Tests
+{
+    [TestClass]
+    public class _205_IsomorphicStringsTests
+    {
+        [TestMethod]
+        public void IsIsomorphicTest()
+        {
+            var tmp = new _205_IsomorphicStrings();
+            Assert.IsTrue(tmp.IsIsomorphic("egg", "add"));
+            Assert.IsFalse(tmp.IsIsomorphic("foo", "bar"));
+            Assert.IsTrue(tmp.IsIsomorphic("paper", "title"));
+            Assert.IsFalse(tmp.IsIsomorphic("ab", "aa"));
+        }
+    }
+}
---
+++ Playthings/LeetCode.Solutions/LeetCode.Solutions.Argorithms/_205_IsomorphicStrings.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections.Generic;
+
+namespace LeetCode.Solutions.Argorithms
+{
+    public class _205_IsomorphicStrings
+    {
+        public bool IsIsomorphic(string s, string t)
+        {
+            var a = new Dictionary<char, char>();
+            for (var i = 0; i < s.Length; i++)
+            {
+                if (a.ContainsKey(s[i]))
+                {
+                    if (a[s[i]] != t[i])
+                    {
+                        return false;
+                    }
+                }
+                else
+                {
+                    if (a.ContainsValue(t[i]))
+                    {
+                        return false;
+                    }
+                    a.Add(s[i], t[i]);
+                }
+            }
+            return true;
+        }
+    }
+}
