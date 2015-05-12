b33314f4908affae04f967b9e5448190974afbb0
york yao <york.yao@dragonsoftbravo.com>
2015-05-12 16:31:13 +0800

    add N Queens

---
+++ Playthings/LeetCode.Solutions/LeetCode.Solutions.Argorithms.Tests/_51_NQueensTests.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace LeetCode.Solutions.Argorithms.Tests
+{
+    [TestClass]
+    public class _51_NQueensTests
+    {
+        [TestMethod]
+        public void SolveNQueensTest()
+        {
+            var tmp = new _51_NQueens();
+            var queens = tmp.SolveNQueens(4);
+        }
+    }
+}
---
+++ Playthings/LeetCode.Solutions/LeetCode.Solutions.Argorithms/_51_NQueens.cs
@@ -0,0 +1,105 @@
+﻿using System.Collections.Generic;
+
+namespace LeetCode.Solutions.Argorithms
+{
+    public class _51_NQueens
+    {
+        public IList<string[]> SolveNQueens(int n)
+        {
+            var result = new List<string[]>();
+            Xxx(result, n, 1, new int[n], 0);
+            return result;
+        }
+
+        private static string[] Format(int n, IList<int> ints)
+        {
+            var s = new string[n];
+            for (var i = 0; i < ints.Count; i++)
+            {
+                s[i] = GetString(n, ints[i]);
+            }
+            return s;
+        }
+
+        private static string GetString(int n, int i)
+        {
+            var tmp = new char[n];
+            for (var j = 0; j < n; j++)
+            {
+                if (j == i)
+                {
+                    tmp[j] = 'Q';
+                }
+                else
+                {
+                    tmp[j] = '.';
+                }
+            }
+            return new string(tmp);
+        }
+
+        private static void Xxx(ICollection<string[]> result, int maxDepth, int depth, int[] queens, int queensLength)
+        {
+            if (maxDepth == 1)
+            {
+                result.Add(Format(maxDepth, queens));
+                return;
+            }
+            for (var i = 0; i < maxDepth; i++)
+            {
+                queens[queensLength] = i;
+                if (depth == 1)
+                {
+                    Xxx(result, maxDepth, depth + 1, queens, queensLength + 1);
+                    continue;
+                }
+                if (!IsValid(queens, queensLength + 1))
+                {
+                    continue;
+                }
+                if (depth == maxDepth)
+                {
+                    result.Add(Format(maxDepth, queens));
+                }
+                else
+                {
+                    Xxx(result, maxDepth, depth + 1, queens, queensLength + 1);
+                }
+            }
+        }
+
+        private static bool IsValid(IList<int> queens, int queensLength)
+        {
+            // the last point
+            var px = queensLength - 1;
+            var py = queens[px];
+
+            for (var i = 0; i < px; i++)
+            {
+                // |
+                if (px == i)
+                {
+                    return false;
+                }
+                // -
+                if (py == queens[i])
+                {
+                    return false;
+                }
+                var deltaX = px - i;
+                var deltaY = py - queens[i];
+                // \
+                if (deltaX == deltaY)
+                {
+                    return false;
+                }
+                // /
+                if (deltaX + deltaY == 0)
+                {
+                    return false;
+                }
+            }
+            return true;
+        }
+    }
+}
