bc40017236fe2ccea0227fd035a8263416d913f5
yaoyao <yaoyao12306@163.com>
2015-05-12 21:58:33 +0800

    save

---
+++ Playthings/LeetCode.Solutions/LeetCode.Solutions.Argorithms.Tests/_118_PascalsTriangleTests.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace LeetCode.Solutions.Argorithms.Tests
+{
+    [TestClass]
+    public class _118_PascalsTriangleTests
+    {
+        [TestMethod]
+        public void GenerateTest()
+        {
+            var tmp = new _118_PascalsTriangle().Generate(5);
+        }
+    }
+}
---
+++ Playthings/LeetCode.Solutions/LeetCode.Solutions.Argorithms/_125_ValidPalindrome.cs
@@ -0,0 +1,37 @@
+﻿namespace LeetCode.Solutions.Argorithms
+{
+    public class _125_ValidPalindrome
+    {
+        public bool IsPalindrome(string s)
+        {
+            if (string.IsNullOrEmpty(s))
+            {
+                return true;
+            }
+            var start = 0;
+            var end = s.Length - 1;
+            while (start <= end)
+            {
+                if (!char.IsLetterOrDigit(s[start]))
+                {
+                    start++;
+                }
+                else if (!char.IsLetterOrDigit(s[end]))
+                {
+                    end--;
+                }
+                else
+                {
+                    if (char.ToLower(s[start]) != char.ToLower(s[end]))
+                    {
+                        return false;
+                    }
+                    start++;
+                    end--;
+                }
+            }
+
+            return true;
+        }
+    }
+}
