0a675d57344e26ae1ea0eb97dc6b054111bbfa45
york yao <york.yao@dragonsoftbravo.com>
2015-05-12 17:22:26 +0800

    add plus one

---
+++ Playthings/LeetCode.Solutions/LeetCode.Solutions.Argorithms.Tests/_66_PlusOneTests.cs
@@ -0,0 +1,18 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace LeetCode.Solutions.Argorithms.Tests
+{
+    [TestClass]
+    public class _66_PlusOneTests
+    {
+        [TestMethod]
+        public void PlusOneTest()
+        {
+            var tmp = new _66_PlusOne();
+            var result = tmp.PlusOne(new[]
+                                     {
+                                         0
+                                     });
+        }
+    }
+}
---
+++ Playthings/LeetCode.Solutions/LeetCode.Solutions.Argorithms/_66_PlusOne.cs
@@ -0,0 +1,30 @@
+﻿namespace LeetCode.Solutions.Argorithms
+{
+    public class _66_PlusOne
+    {
+        public int[] PlusOne(int[] digits)
+        {
+            for (var i = digits.Length - 1; i >= 0; i--)
+            {
+                if (digits[i] != 9)
+                {
+                    digits[i]++;
+                    break;
+                }
+                digits[i] = 0;
+            }
+
+            if (digits[0] != 0)
+            {
+                return digits;
+            }
+            var result = new int[digits.Length + 1];
+            result[0] = 1;
+            for (var i = 0; i < digits.Length; i++)
+            {
+                result[i + 1] = digits[i];
+            }
+            return result;
+        }
+    }
+}
