9a7a8172e40fd702a2117dd2f7c92f2b62edb8e9
york yao <york.yao@dragonsoftbravo.com>
2015-05-12 14:55:07 +0800

    add trie

---
+++ Playthings/LeetCode.Solutions/LeetCode.Solutions.Argorithms/TrieNode.cs
@@ -0,0 +1,68 @@
+﻿namespace LeetCode.Solutions.Argorithms
+{
+    public class TrieNode
+    {
+        public bool IsWord { get; set; }
+
+        public TrieNode[] Children { get; set; }
+
+        public void Insert(string word, int index)
+        {
+            if (index == word.Length)
+            {
+                IsWord = true;
+                return;
+            }
+
+            var i = word[index] - 'a';
+
+            if (Children == null)
+            {
+                Children = new TrieNode[26];
+            }
+
+            if (Children[i] == null)
+            {
+                Children[i] = new TrieNode();
+            }
+
+            Children[i].Insert(word, index + 1);
+        }
+
+        public bool Search(string word, int index)
+        {
+            if (index == word.Length)
+            {
+                return IsWord;
+            }
+
+            var i = word[index] - 'a';
+
+            if (Children == null
+                || Children[i] == null)
+            {
+                return false;
+            }
+
+            return Children[i].Search(word, index + 1);
+        }
+
+        public bool StartsWith(string word, int index)
+        {
+            if (index == word.Length)
+            {
+                return true;
+            }
+
+            var i = word[index] - 'a';
+
+            if (Children == null
+                || Children[i] == null)
+            {
+                return false;
+            }
+
+            return Children[i].StartsWith(word, index + 1);
+        }
+    }
+}
