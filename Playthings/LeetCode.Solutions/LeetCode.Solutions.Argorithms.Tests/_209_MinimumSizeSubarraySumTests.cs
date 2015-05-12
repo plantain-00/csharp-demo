20be7fdf24d5bcac068f98ffd7f3b410f2539a1f
york yao <york.yao@dragonsoftbravo.com>
2015-05-12 15:35:50 +0800

    add Minimum Size Subarray Sum

---
+++ Playthings/LeetCode.Solutions/LeetCode.Solutions.Argorithms.Tests/_209_MinimumSizeSubarraySumTests.cs
@@ -0,0 +1,24 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace LeetCode.Solutions.Argorithms.Tests
+{
+    [TestClass]
+    public class _209_MinimumSizeSubarraySumTests
+    {
+        [TestMethod]
+        public void MinSubArrayLenTest()
+        {
+            var tmp = new _209_MinimumSizeSubarraySum();
+            Assert.IsTrue(tmp.MinSubArrayLen(7,
+                                             new[]
+                                             {
+                                                 2,
+                                                 3,
+                                                 1,
+                                                 2,
+                                                 4,
+                                                 3
+                                             }) == 2);
+        }
+    }
+}
---
+++ Playthings/LeetCode.Solutions/LeetCode.Solutions.Argorithms/_209_MinimumSizeSubarraySum.cs
@@ -0,0 +1,43 @@
+﻿using System;
+
+namespace LeetCode.Solutions.Argorithms
+{
+    public class _209_MinimumSizeSubarraySum
+    {
+        public int MinSubArrayLen(int s, int[] nums)
+        {
+            if (nums.Length == 0)
+            {
+                return 0;
+            }
+            var first = 0;
+            var second = 0;
+            var min = nums.Length + 1;
+            var sum = nums[0];
+            while (first < nums.Length
+                   && second <= first)
+            {
+                if (sum < s)
+                {
+                    first++;
+                    if (first < nums.Length)
+                    {
+                        sum += nums[first];
+                    }
+                }
+                else
+                {
+                    min = Math.Min(first - second + 1, min);
+                    sum -= nums[second];
+                    second++;
+                }
+            }
+
+            if (min == nums.Length + 1)
+            {
+                return 0;
+            }
+            return min;
+        }
+    }
+}
