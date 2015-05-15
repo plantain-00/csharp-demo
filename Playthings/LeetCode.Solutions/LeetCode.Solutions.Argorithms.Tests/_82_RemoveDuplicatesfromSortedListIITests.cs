c8d938f96471c3c6aa7bddb9ccc2b66392dee672
york yao <york.yao@dragonsoftbravo.com>
2015-05-15 15:52:37 +0800

    add 82/83

---
+++ Playthings/LeetCode.Solutions/LeetCode.Solutions.Argorithms.Tests/_82_RemoveDuplicatesfromSortedListIITests.cs
@@ -0,0 +1,27 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace LeetCode.Solutions.Argorithms.Tests
+{
+    [TestClass]
+    public class _82_RemoveDuplicatesfromSortedListIITests
+    {
+        [TestMethod]
+        public void DeleteDuplicatesTest()
+        {
+            var tmp = new ListNode(1)
+                      {
+                          next = new ListNode(1)
+                                 {
+                                     next = new ListNode(2)
+                                            {
+                                                next = new ListNode(3)
+                                                       {
+                                                           next = new ListNode(3)
+                                                       }
+                                            }
+                                 }
+                      };
+            var a = new _82_RemoveDuplicatesfromSortedListII().DeleteDuplicates(tmp);
+        }
+    }
+}
---
+++ Playthings/LeetCode.Solutions/LeetCode.Solutions.Argorithms/_83_RemoveDuplicatesfromSortedList.cs
@@ -0,0 +1,25 @@
+﻿namespace LeetCode.Solutions.Argorithms
+{
+    public class _83_RemoveDuplicatesfromSortedList
+    {
+        public ListNode DeleteDuplicates(ListNode head)
+        {
+            var current = head;
+
+            while (current != null
+                   && current.next != null)
+            {
+                if (current.val == current.next.val)
+                {
+                    current.next = current.next.next;
+                }
+                else
+                {
+                    current = current.next;
+                }
+            }
+
+            return head;
+        }
+    }
+}
