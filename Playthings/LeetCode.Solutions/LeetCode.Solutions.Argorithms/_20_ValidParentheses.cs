b358f9d5c371984b860c497842d5bb23e03be69b
york yao <york.yao@dragonsoftbravo.com>
2015-05-15 18:18:05 +0800

    add 20,21

---
+++ Playthings/LeetCode.Solutions/LeetCode.Solutions.Argorithms/_20_ValidParentheses.cs
@@ -0,0 +1,54 @@
+﻿using System.Collections.Generic;
+
+namespace LeetCode.Solutions.Argorithms
+{
+    public class _20_ValidParentheses
+    {
+        public bool IsValid(string s)
+        {
+            var stack = new Stack<char>();
+            foreach (var c in s)
+            {
+                switch (c)
+                {
+                    case '{':
+                    case '(':
+                    case '[':
+                        stack.Push(c);
+                        break;
+                    case '}':
+                        if (stack.Count == 0)
+                        {
+                            return false;
+                        }
+                        if (stack.Pop() != '{')
+                        {
+                            return false;
+                        }
+                        break;
+                    case ')':
+                        if (stack.Count == 0)
+                        {
+                            return false;
+                        }
+                        if (stack.Pop() != '(')
+                        {
+                            return false;
+                        }
+                        break;
+                    case ']':
+                        if (stack.Count == 0)
+                        {
+                            return false;
+                        }
+                        if (stack.Pop() != '[')
+                        {
+                            return false;
+                        }
+                        break;
+                }
+            }
+            return stack.Count == 0;
+        }
+    }
+}
