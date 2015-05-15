a2b5d0c65e13baab5463218ebe68b4ecc28cd1d4
york yao <york.yao@dragonsoftbravo.com>
2015-05-15 15:21:13 +0800

    save

---
+++ Playthings/LeetCode.Solutions/LeetCode.Solutions.Argorithms/_88_MergeSortedArray.cs
@@ -0,0 +1,23 @@
+﻿namespace LeetCode.Solutions.Argorithms
+{
+    public class _88_MergeSortedArray
+    {
+        public void Merge(int[] nums1, int m, int[] nums2, int n)
+        {
+            while (n > 0)
+            {
+                if (m == 0
+                    || nums2[n - 1] > nums1[m - 1])
+                {
+                    n--;
+                    nums1[m + n] = nums2[n];
+                }
+                else
+                {
+                    m--;
+                    nums1[m + n] = nums1[m];
+                }
+            }
+        }
+    }
+}
