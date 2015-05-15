39b32e4ce730e4328e95b7a6cbba25fbaaf204e1
york yao <york.yao@dragonsoftbravo.com>
2015-05-15 16:15:23 +0800

    add 58,70

---
+++ Playthings/LeetCode.Solutions/LeetCode.Solutions.Argorithms.Tests/_58_LengthofLastWordTests.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace LeetCode.Solutions.Argorithms.Tests
+{
+    [TestClass]
+    public class _58_LengthofLastWordTests
+    {
+        [TestMethod]
+        public void LengthOfLastWordTest()
+        {
+            var a = new _58_LengthofLastWord().LengthOfLastWord("Hello World");
+        }
+    }
+}
---
+++ Playthings/LeetCode.Solutions/LeetCode.Solutions.Argorithms/_58_LengthofLastWord.cs
@@ -0,0 +1,17 @@
+﻿namespace LeetCode.Solutions.Argorithms
+{
+    public class _58_LengthofLastWord
+    {
+        public int LengthOfLastWord(string s)
+        {
+            if (string.IsNullOrEmpty(s))
+            {
+                return 0;
+            }
+
+            s = s.TrimEnd();
+
+            return s.Length - 1 - s.LastIndexOf(' ');
+        }
+    }
+}
