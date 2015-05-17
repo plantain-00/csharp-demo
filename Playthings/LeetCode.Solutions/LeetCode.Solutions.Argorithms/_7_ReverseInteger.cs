f7af4908631b3f5070ff5ecdf550c6b1ce2f6ee5
yaoyao <yaoyao12306@163.com>
2015-05-17 18:30:28 +0800

    save

---
+++ Playthings/LeetCode.Solutions/LeetCode.Solutions.Argorithms/_7_ReverseInteger.cs
@@ -0,0 +1,23 @@
+﻿namespace LeetCode.Solutions.Argorithms
+{
+    public class _7_ReverseInteger
+    {
+        public int Reverse(int x)
+        {
+            long result = 0;
+            do
+            {
+                result = x % 10 + result * 10;
+                x = x / 10;
+            }
+            while (x != 0);
+
+            if (result > int.MaxValue
+                || result < int.MinValue)
+            {
+                result = 0;
+            }
+            return (int) result;
+        }
+    }
+}
