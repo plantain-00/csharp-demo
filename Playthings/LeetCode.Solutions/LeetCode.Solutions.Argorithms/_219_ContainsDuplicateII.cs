1215798dabbd3381eb22c749e24709dd2a109ed7
york yao <york.yao@dragonsoftbravo.com>
2015-06-05 17:22:10 +0800

    add 219

---
+++ Playthings/LeetCode.Solutions/LeetCode.Solutions.Argorithms/_219_ContainsDuplicateII.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections.Generic;
+
+namespace LeetCode.Solutions.Argorithms
+{
+    public class _219_ContainsDuplicateII
+    {
+        public bool ContainsNearbyDuplicate(int[] nums, int k)
+        {
+            var set = new HashSet<int>();
+
+            for (var i = 0; i < nums.Length && i <= k; i++)
+            {
+                if (!set.Add(nums[i]))
+                {
+                    return true;
+                }
+            }
+
+            for (var i = k + 1; i < nums.Length; i++)
+            {
+                set.Remove(nums[i - k - 1]);
+                if (!set.Add(nums[i]))
+                {
+                    return true;
+                }
+            }
+            return false;
+        }
+    }
+}
